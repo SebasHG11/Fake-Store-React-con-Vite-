f867e8cbc356c8b8d992dda51796e4443e98f001
Higuita <sebastian.higuita@softwareone.com>
2024-07-19 15:51:15 -0500

    Agrego buscador para filtrar productos

---
+++ src/Componentes/BusquedaProductos/index.jsx
@@ -0,0 +1,18 @@
+import '../BusquedaProductos/styles.css'
+
+export const BusquedaProductos = ({busqueda, setBusqueda}) =>{
+    return(
+        <div className='container-busqueda-productos'>
+            <label htmlFor="inputSearch">Buscar</label>
+            <input 
+            name="inputSearch"
+            type="text"
+            placeholder='Ingresa un producto...'
+            value={busqueda}
+            onChange={(event)=>{
+                setBusqueda(event.target.value)
+            }}
+            />
+        </div>
+    )
+}
--- src/Paginas/Home/index.jsx
+++ src/Paginas/Home/index.jsx
@@ -1,11 +1,12 @@
-import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { ContainerCards } from '../../Componentes/ContainerCards'
 import { Card } from '../../Componentes/Card'
 import { useFetchData } from '../../Helpers/useFetchData'
+import { BusquedaProductos } from '../../Componentes/BusquedaProductos'
 
 export const Home = () =>{
   const[items, setItems] = useState([])
+  const[busqueda, setBusqueda] = useState('')
 
   const {data, error} = useFetchData('http://localhost:5164/api/Producto')
   
@@ -15,14 +16,24 @@ export const Home = () =>{
     :
     console.log(error)
   }
-  
+
+  const arrayBusqueda = items.filter(item=>{
+    return item.nombre.toLowerCase().includes(busqueda.toLowerCase())
+  })
+
   return(
-    <ContainerCards>
-      {items &&
-        items.map((p)=>(
-          <Card key={p.id} producto={p} />
-        ))
-      }
-    </ContainerCards>
+    <>
+    <BusquedaProductos 
+    busqueda={busqueda}
+    setBusqueda={setBusqueda}
+    />
+      <ContainerCards>
+        {arrayBusqueda &&
+          arrayBusqueda.map((p)=>(
+            <Card key={p.id} producto={p} />
+          ))
+        }
+      </ContainerCards>
+    </>
   )
 }
